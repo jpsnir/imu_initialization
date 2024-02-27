91acbed730cfe824f6b2ee3250f2b4a95abac34e
jagatpreet <nir.j@northeastern.edu>
2024-02-27 02:57:32 -0500

    added tests folder

---
+++ tests/test_experiment02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(find_closest, upper_bound_test){
+
+};
+
+int main(int argc, char ** argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
