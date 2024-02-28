4f14a05ed1ec8f8f93ede13658e4fb0e01977447
jagatpreet <nir.j@northeastern.edu>
2024-02-28 08:51:26 -0500

    refactored experiment2 - created header file for testing

--- tests/test_experiment02.cpp
+++ tests/test_experiment02.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <experiment02.h>
 
 using namespace std;
 
 TEST(find_closest, upper_bound_test){
 
 };
+TEST(read_file, TUM_file_format_test){
 
-int main(int argc, char ** argv){
+}
+
+int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
